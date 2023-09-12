4f3d820a8168915c1cfdc0d34161bc0f6a34ba9e
Huiyu <102457333+hyyeo@users.noreply.github.com>
2023-09-12 16:26:56 +0800

    [DVS-009] updated smartbed

--- models/smartbed.js
+++ models/smartbed.js
@@ -4,11 +4,11 @@ const bedStatusEnum = ['occupied','vacant', 'ready'];
 
 const smartBedSchema = new mongoose.Schema({
   bedNum: {
-    type: Number,
+    type: String,
     required: true,
   },
   roomNum: {
-    type: Number,
+    type: String,
     required: true,
   },
   bedStatus: {
