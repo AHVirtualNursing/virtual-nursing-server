329b5cfdc9dbf4c2de3d138bbcbb97be6c3acd2b
gwajoon <hengguojun25@gmail.com>
2023-09-20 23:43:36 +0800

    Add beds to ward model

--- models/ward.js
+++ models/ward.js
@@ -1,41 +1,67 @@
-const mongoose = require('mongoose');
-const wardTypeEnum = ['A1', 'B1', 'B2', 'C'];
+const mongoose = require("mongoose");
+const wardTypeEnum = ["A1", "B1", "B2", "C"];
 
-
-
-const wardSchema = new mongoose.Schema({
+const wardSchema = new mongoose.Schema(
+  {
     wardNum: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     wardType: {
-        type: String,
-        enum: {
-            values: wardTypeEnum,
-            message: 'Invalid ward type status: {VALUE}',
-        },
-        required: false,
+      type: String,
+      enum: {
+        values: wardTypeEnum,
+        message: "Invalid ward type status: {VALUE}",
+      },
+      required: false,
     },
     numRooms: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     smartBeds: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:'SmartBed'
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SmartBed",
+      },
     ],
     nurses: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:'Nurse'    
-        }
-    ]},
-    {
-        timestamps: true
-    });
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Nurse",
+      },
+    ],
+    beds: [
+      {
+        type: Number,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
+wardSchema.pre("save", async function (next) {
+  const ward = this;
+  const wardType = ward.wardType;
 
-module.exports = mongoose.models.Ward || mongoose.model('Ward', wardSchema);
+  function getBedsPerRoom() {
+    switch (wardType) {
+      case "A1":
+        return 1;
+      case "B1":
+        return 4;
+      case "B2":
+        return 5;
+      case "C":
+        return 5;
+    }
+  }
 
+  const numBeds = getBedsPerRoom() * ward.numRooms;
+
+  ward.beds = new Array(numBeds).fill(0);
+});
+
+module.exports = mongoose.models.Ward || mongoose.model("Ward", wardSchema);
