194c7a23ae36501a3905c02cc8be3bd75e28d345
seanangwj <78225993+seanangwj@users.noreply.github.com>
2023-11-10 17:04:25 +0800

    auth controller

---
+++ controllers/authenticationController.js
@@ -0,0 +1,95 @@
+const mongoose = require("mongoose");
+const passport = require("../middleware/passport");
+const generator = require("generate-password");
+const NurseController = require("./nurseController");
+const VirtualNurseController = require("./virtualNurseController");
+const { validationResult } = require("express-validator");
+const { getUserModel } = require("../helper/auth");
+const { sendWelcomeEmail } = require("../helper/email");
+
+
+const register = async(req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const userType = req.headers["x-usertype"];
+    const userModel = getUserModel(userType);
+
+    const { name, username, email } = req.body;
+
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    let password = generator.generate({
+      length: 8,
+      numbers: true,
+      symbols: true,
+      excludeSimilarCharacters: true,
+      strict: true,
+    });
+    if (req.query.default) {
+      req.body.password = "password";
+      password = "password";
+    } else {
+      req.body.password = password;
+    }
+    try {
+      let newUser;
+      if (userType == "mobile") {
+        newUser = await NurseController.createNurse(req, res, session);
+      } else if (userType == "virtual-nurse") {
+        newUser = await VirtualNurseController.createVirtualNurse(req, res);
+      } else {
+        newUser = new userModel({ name, username, email, password });
+      }
+      if (req.query.default) {
+        newUser.passwordReset = true;
+      }
+      await newUser.save({ session });
+      await sendWelcomeEmail(email, username, password);
+      await session.commitTransaction();
+      session.endSession();
+      res.status(201).json({ data: { _id: newUser._id } });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+}
+
+const login = async(req, res, next) => {
+
+    passport.authenticate("local", (err, user) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "An error occurred during login." });
+        }
+        if (!user) {
+          return res.status(401).json({ message: "Invalid email or password." });
+        }
+        req.login(user, (err) => {
+          if (err) {
+            console.error(err);
+            return res
+              .status(500)
+              .json({ message: "An error occurred during login." });
+          }
+          return res.status(200).json({ message: "Login successful", user: user });
+        });
+      })(req, res, next);
+}
+
+const logout = async(req, res) => {
+    req.logout();
+    res.status(200).json({ message: "Logged out successfully" });
+}
+
+module.exports = {register, login, logout};
